a7f184af7327338f0a9242ad97523a4fa3ae831e
AgathaBahati <matinodev@gmail.com>
2024-07-27 21:19:45 +0300

    refactor: modify docker compose

--- db/db.py
+++ db/db.py
@@ -7,8 +7,8 @@ from .. import models  # noqa: F401
 settings = Settings()
 
 DATABASE_URL = (
-    f"postgresql://{settings.db_user}:{settings.db_password}"
-    f"@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+    f"postgresql://{settings.postgres_user}:{settings.postgres_password}"
+    f"@{settings.db_host}/{settings.postgres_db}"
 )
 
 engine = create_engine(DATABASE_URL, echo=True)
