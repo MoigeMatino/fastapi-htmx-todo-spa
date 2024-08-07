a0647f768119485d99ecf0ae84004079284304f7
AgathaBahati <matinodev@gmail.com>
2024-08-07 22:36:49 +0300

    test: add test_update_todo

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -24,3 +24,12 @@ def test_create_todo(client):
     response = client.post("/todos", data={"todo": "New Todo"})
     assert response.status_code == 200
     assert "New Todo" in response.text
+
+
+def test_update_todo(client, override_session):
+    todo_data = TodoCreate(title="Old Title")
+    todo = db_create_todo(override_session, todo_data)
+
+    response = client.put(f"/todos/{todo.id}", data={"title": "New Title"})
+    assert response.status_code == 200
+    assert "New Title" in response.text
