4f5bc28b096acdc9a82a3fb5781ccbb2d662f124
AgathaBahati <matinodev@gmail.com>
2024-08-23 11:34:34 +0300

    fix: fix payload data to endpoint as form data not json

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,13 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
 from sqlmodel import Session
 
 from app.db import get_session
-from app.models.user import UserCreate, UserResponse
+from app.models.user import UserResponse
 from app.utils.jwt import create_access_token
 from app.utils.user import authenticate_user, create_user_in_db, get_user_by_username
 
@@ -16,16 +16,20 @@ templates = Jinja2Templates(directory="app/templates")
 
 
 @router.post("/signup", response_model=UserResponse)
-def signup(user: UserCreate, session: Session = Depends(get_session)):
+def signup(
+    username: str = Form(...),
+    password: str = Form(...),
+    session: Session = Depends(get_session),
+):
     # check if user exists
-    existing_user = get_user_by_username(user.username, session)
+    existing_user = get_user_by_username(username, session)
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken"
         )
 
     # if user doesn't exist, create user in the database
-    new_user = create_user_in_db(user.username, user.password, session)
+    new_user = create_user_in_db(username, password, session)
     return UserResponse(id=new_user.id, username=new_user.username)
 
 
