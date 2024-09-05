10f0416715880ab9982595d4b39f4cc5a6f4b89f
AgathaBahati <matinodev@gmail.com>
2024-09-05 23:11:59 +0300

    refactor: modularise auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -10,26 +10,35 @@ def test_signup(client):
 
 
 def test_signup_existing_user(client):
+    # initial signup request made by newuser
+    response = client.post(
+        "/auth/signup", data={"username": "newuser", "password": "password123"}
+    )
+
+    # Second signup request made by existing user
     response = client.post(
         "/auth/signup", data={"username": "newuser", "password": "password123"}
     )
     assert response.status_code == 400
     assert response.json() == {"detail": "Username already taken"}
 
-    # Test with missing username
+
+def test_signup_missing_username(client):
     response = client.post("/auth/signup", data={"password": "password123"})
 
     assert response.status_code == 422
     assert "detail" in response.json()
     assert "Field required" in response.json()["detail"][0]["msg"]
 
-    # Test with missing password
+
+def test_signup_missing_password(client):
     response = client.post("/auth/signup", data={"username": "newuser"})
     assert response.status_code == 422
     assert "detail" in response.json()
     assert "Field required" in response.json()["detail"][0]["msg"]
 
-    # Test with empty data
+
+def test_signup_empty_data(client):
     response = client.post("/auth/signup", data={})
 
     assert response.status_code == 422
@@ -38,16 +47,15 @@ def test_signup_existing_user(client):
     assert response.json()["detail"][1]["loc"] == ["body", "password"]
 
 
-# def test_login_(client, override_session):
+# def test_login(client, override_session):
 #     create_user_in_db("test_user","password123", override_session)
 #     # Test with valid credentials
 #     response = client.post(
 #         "/auth/login", data={"username": "test_user", "password": "password123"}
 #     )
 #     import pdb; pdb.set_trace()
-#     assert response.status_code == 303
-#     assert response.headers["Location"] == "http://localhost/?username=test_user"
-#     assert "Authorization" in response.cookies
+#     assert response.status_code == 200
+
 
 #     # Test with invalid password
 #     response = client.post(
