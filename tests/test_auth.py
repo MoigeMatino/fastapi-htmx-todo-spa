9b449be08da1b77f26c42a50a14864e76c5ea7d6
AgathaBahati <matinodev@gmail.com>
2024-09-05 22:58:15 +0300

    refactor: modularise auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,6 @@
+from app.utils.user import create_user_in_db  # noqa: F401
+
+
 def test_signup(client):
     response = client.post(
         "/auth/signup", data={"username": "newuser", "password": "password123"}
@@ -5,7 +8,8 @@ def test_signup(client):
     assert response.status_code == 200
     assert response.json()["success"]
 
-    # test signup for an existing user
+
+def test_signup_existing_user(client):
     response = client.post(
         "/auth/signup", data={"username": "newuser", "password": "password123"}
     )
@@ -32,3 +36,21 @@ def test_signup(client):
     assert "detail" in response.json()
     assert response.json()["detail"][0]["loc"] == ["body", "username"]
     assert response.json()["detail"][1]["loc"] == ["body", "password"]
+
+
+# def test_login_(client, override_session):
+#     create_user_in_db("test_user","password123", override_session)
+#     # Test with valid credentials
+#     response = client.post(
+#         "/auth/login", data={"username": "test_user", "password": "password123"}
+#     )
+#     import pdb; pdb.set_trace()
+#     assert response.status_code == 303
+#     assert response.headers["Location"] == "http://localhost/?username=test_user"
+#     assert "Authorization" in response.cookies
+
+#     # Test with invalid password
+#     response = client.post(
+#         "/auth/login", data={"username": "test_user", "password": "password124"}
+#     )
+#     import pdb; pdb.set_trace()
